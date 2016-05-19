cc505c97d274be898289f71496876f61e09e93d3
Bruce Mitchener <bruce.mitchener@gmail.com>
2016-05-19 11:27:36 +0700

    Add doc comments.

--- src/command.ts
+++ src/command.ts
@@ -1,5 +1,7 @@
 import { OutputRecord } from "./output_record";
 
+/** A command */
 export interface Command {
+  /** Execute the command. */
   execute(): Promise<OutputRecord>;
 }
--- src/command_table.ts
+++ src/command_table.ts
@@ -1,13 +1,30 @@
 import { Command } from "./command";
 
+/**
+ * Information about a command in a command table.
+ */
 export class CommandTableEntry {
+  /** Name of a command. This is used for command line entry. */
   name: string;
+  /** The underlying command that can be executed. */
   command: Command;
 }
 
+/**
+ * A group of related commands.
+ *
+ * Command tables are a way of grouping a set of related command
+ * objects and providing user interfaces to the commands.
+ *
+ * Command tables can be interacted with via menus, toolbars,
+ * keystrokes, gestures and more.
+ */
 export class CommandTable {
+  /** The name of the command table. */
   private name: string;
+  /** Tables inherited by this table. */
   private inherit: CommandTable[] = [];
+  /** Commands in this table. */
   private commands: CommandTableEntry[] = [];
 
   constructor (name: string, inherit: CommandTable[]) {
